0214db15b76fda1f0a37f2cbbb9fc04f9b1a184f
alto <victor1301166040@gmail.com>
2021-08-25 12:26:52 -0700

    d

---
+++ Auto_Diff/Quantile_Network/quantile_net.jl
@@ -0,0 +1,3 @@
+### A regression neural network that fits on the quantiles of distributions at the same time. 
+### Remember this, a neuro network is a bunch of sub networks where each one is capable of predicting stuff. 
+
