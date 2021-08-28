8652df62daddacb83e0a9f566e9238583ca1e65e
alto <victor1301166040@gmail.com>
2021-08-28 14:46:00 -0700

    d

--- Inexact_Dual_Numerics/prototype2.jl
+++ Inexact_Dual_Numerics/prototype2.jl
@@ -1,20 +1,13 @@
-### The absolute error is quantized. 
+### The absolute error is modeled exactly, just like a real number, but this might
+### Not be realistic. 
 
-struct InexactFloat <: AbstractFloat
-    x::Float64    # Original number 
-    δ::Float64    # Absolute error
-    ϵ::Float64    # Relative Error
+struct InexactFloat <: Real
+    x::Float64 # Original number 
+    δ::Float64 # Absolute error
+    ϵ::Float64 # Relative Error
 
-    function InexactFloat()
-        return InexactFloat(0.0)
-    end
-    
     function InexactFloat(a::Float64)
-        if a == 0.0
-            δ = 0
-        else
-            δ = 2.0^(exponent(a) + exponent(eps(Float64)) - 1)
-        end
+        δ = eps(Float64)*abs(a)/2
         ϵ = δ/abs(a)
         x = a
         return new(x, δ, ϵ)
@@ -35,8 +28,9 @@ function Base.:+(a::InexactFloat, b::InexactFloat)
         return b + a  # WLOG: a > b
     end
     c = a.x + b.x
-    # Loss of significance cancellation.
-    δ = a.δ + b.δ + 2.0^(exponent(a.x) + exponent(eps(Float64)) - 1)
+    # use rel error and consider loss of significance
+    # ϵ = (a.ϵ*a.x + b.ϵ*b.x)/(a.x + b.x) + 2^(-54)
+    δ = a.δ + b.δ + eps(Float64)*abs(a.x)/2
     ϵ = δ/abs(c)
     return InexactFloat(c, ϵ)
 end
@@ -51,7 +45,7 @@ end
 
 function Base.:-(a::InexactFloat, b::InexactFloat)
     c = a.x - b.x
-    δ = a.δ + b.δ + 2.0^(exponent(a.x) + exponent(eps(Float64)) - 1)
+    δ = a.δ + b.δ + eps(Float64)*abs(a.x)/2
     ϵ = δ/abs(c)
     return InexactFloat(c, ϵ)
 end
@@ -80,14 +74,12 @@ function Base.:*(a::InexactFloat, b::InexactFloat)
 end
 function Base.:*(a::InexactFloat, b::Real)
     c = a.x*b
-    ϵ = a.ϵ + 2.0^(exponent(eps(Float64)) - 1) + 
-        a.ϵ*2.0^(exponent(eps(Float64)) - 1)
+    ϵ = a.ϵ
     return InexactFloat(c, ϵ)
 end
 function Base.:*(b::Real, a::InexactFloat)
     c = a.x*b
-    ϵ = a.ϵ + 2.0^(exponent(eps(Float64)) - 1) + 
-    a.ϵ*2.0^(exponent(eps(Float64)) - 1)
+    ϵ = a.ϵ
     return InexactFloat(c, ϵ)
 end
 
@@ -121,56 +113,22 @@ function Base.:^(a::InexactFloat, k::Integer)
     return InexactFloat(c, ϵ)
 end
 
-# TODO: Integer Division
-# TODO: Power is also Inexact 
-
-
 # Oderness, equality of the number ---------------------------------------------
-# This will be needed to run the linear algebra libraries and other common
-# Numerical Algorithms. 
 
 
 # Comon Functions on this type of numbers --------------------------------------
 
 
+# The special element in the field ---------------------------------------------
 
-
-# The special functons ---------------------------------------------------------
-
-function Base.zero(::Type{InexactFloat})
-    return InexactFloat(0.0, 0.0)
+function Base.zero(a::InexactFloat)
+    return InexactFloat(0, 0)
 end
 
 function Base.show(io::IO, this::InexactFloat)
     show(io, "$(this.x) ± $(this.δ)")
 end
 
-function Base.:∈(a::Real, b::InexactFloat)
+function Base.:∈(a::Number, b::InexactFloat)
     return a <= b.x + b.δ && a >= b.x - b.δ
 end
-
-function Base.:∈(a::InexactFloat, b::InexactFloat)
-    return a.x + a.δ <= b.x + b.δ && a.x - a.δ >= b.x - b.δ
-end
-
-function Base.:≈(a::Real, b::InexactFloat)
-    return a ∈ b
-end
-
-function Base.convert(::Type{InexactFloat}, a::Float64)
-    return InexactFloat(a)
-end
-function Base.convert(::Type{InexactFloat}, a::InexactFloat)
-    return a
-end
-function Base.convert(::Type{InexactFloat}, a::Integer)
-    return InexactFloat(a + 0.0)
-end
-
-function Base.convert(::Type{Float64}, a::InexactFloat)
-    return a.x
-end
-function Base.convert(::Type{Integer}, a::Integer)
-    return convert(Integer, a.x)
-end
-
--- Inexact_Dual_Numerics/prototype_test.jl
+++ Inexact_Dual_Numerics/prototype_test.jl
@@ -1,22 +1,106 @@
+using Test
 include("prototype.jl")
 
-for II in 1:1000
-    inExactList = [InexactFloat(a)*2^56 for a ∈ rand(2^10)]
-    exactList = [Rational{BigInt}(a.x) for a ∈ inExactList]
-    sum1 = sum(inExactList)
-    sum2 = sum(exactList)
-    println("Inexact Number yeild: $(sum1)")
-    println("Exact Rational is: $(sum2)")
-    println("difference is: $(abs(sum1.x - float(sum2)))")
-    println("Is it in the error bound: $(float(sum2) ∈ sum1)")
-    @assert float(sum2) ∈ sum1 "Ok one instance broke. "
+function BasicSummation()
+
+    for _ in 1:2^10
+        inExactList = [InexactFloat(a*2.0^64) for a ∈ rand(2^4)]
+        exactList = [Rational{BigInt}(a.x) for a ∈ inExactList]
+        sum1 = sum(inExactList)
+        sum2 = sum(exactList)
+        # println("Inexact Number yeild: $(sum1)")
+        # println("Exact Rational is: $(sum2)")
+        # println("difference is: $(abs(sum1.x - float(sum2)))")
+        # println("Is it in the error bound: $(float(sum2) ∈ sum1)")
+        @assert float(sum2) ∈ sum1 "Ok one instance broke. "
+    end
+    return true
+end
+
+function BasicFiniteDiff()
+    f(x) = 1 + 5x + 10x^2 + 10x^3 + 5x^4 + x^5
+
+    Δx = eps(Float64)*2
+    δf(x) = (f(x + Δx) - f(x))/Δx
+    result1 = δf(InexactFloat(2.0))
+    result2 = δf(Rational{BigInt}(2))
+    @assert result2 ∈ result1 "Insnce Faile"* 
+    "Number $(result2) is not in the error range of $(result1)"
+
+    Δx = 1e-8
+    δf(x) = (f(x + Δx) - f(x))/Δx
+    result1 = δf(InexactFloat(2.0))
+    result2 = δf(Rational{BigInt}(2))
+    @assert result2 ∈ result1 "Insnce Faile"* 
+    "Number $(result2) is not in the error range of $(result1)"
+
+    return true
+
+end
+
+function BasicPolynomial()
+    # Higher degree Polynomial
+    p(x, deg=10) = sum([binomial(deg, KK)*x^KK for KK ∈ 0:deg]) # bad poly 
+    q(x, deg=10) = (1 + x)^deg  # Good poly
+    x = 1e-4
+    @assert p(x) ∈ p(InexactFloat(x)) "This instance broke"
+    @assert q(x) ∈ p(InexactFloat(x)) "This instance broke "
+    return true
+end
+
+@testset "Basic" begin
+    @test BasicSummation()
+    @test BasicFiniteDiff()
+    @test BasicPolynomial()
 end
 
 
-f(x) = x^2 + 2x + 1
-Δx = 1e-15
-δf(x) = (f(x - Δx) - f(x))/Δx
-println(δf(InexactFloat(4.0)))
-Δx = 1e-10
-δf(x) = (f(x - Δx) - f(x))/Δx
-println(δf(InexactFloat(2.0)))
+"""
+    Does recursive summation asserts better numerical accuracy, according to
+    our inexact number. 
+
+"""
+function TestRecursiveSummation()
+    N = 2^22
+    function RecursiveSum(
+        arr::Vector, 
+        ii::Union{Nothing, Int64}=nothing, 
+        jj::Union{Nothing, Int64}=nothing
+    )    
+        if ii === nothing
+            ii = 1
+            jj = length(arr)
+        end
+        if jj - ii <= 1
+            if ii == jj
+                return arr[ii]
+            end
+            return arr[ii] + arr[jj]
+        end
+
+        m = (ii + jj)÷2
+        leftSum = RecursiveSum(arr, ii, m)
+        rightSum = RecursiveSum(arr, m + 1, jj)
+        return leftSum + rightSum
+    end
+    toSum = [InexactFloat(II) for II in rand(N)]
+    theSum1 = RecursiveSum(toSum)
+    theSum2 = sum(toSum)
+    theSum3 = sum([Rational{BigInt}(a.x) for a ∈ toSum])
+    println("Using Recursive Sum: $(theSum1)")
+    println("Using reduction Sum: $(theSum2)")
+    println("Using the rational sum $(theSum3)")
+    println("Abs and relative error for recursive and reduction sum are: ")
+    for Sum ∈ [theSum1, theSum2]
+        println("RelError: $((Sum.x - theSum3)/theSum3)")
+        println("AbsError: $(Sum.x - theSum3)")
+    end
+    return true
+end
+
+@testset begin 
+
+    @test TestRecursiveSummation()
+end
+
+ 
