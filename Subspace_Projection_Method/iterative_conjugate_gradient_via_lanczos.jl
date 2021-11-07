81647d7e85456f3a113438b9a748a69e37207cdf
Alto Legato <victor1301166040@gmail.com>
2021-11-07 00:21:11 -0700

    -+

--- Subspace_Projection_Method/iterative_conjugate_gradient_via_lanczos.jl
+++ Subspace_Projection_Method/iterative_conjugate_gradient_via_lanczos.jl
@@ -2,45 +2,57 @@ include("iterative_lanczos.jl")
 
 # The definition of the Iterative Conjugate Gradient method ====================
 #   * only supports real numbers. 
-#   * Uses the Iterative Hesseberg that stores the previous 2 orthogonal vectors. 
-mutable struct IterativeCGViaArnoldi
+#   * Uses the Iterative Lanczos with LDL partially stored. 
+#   
+mutable struct IterativeCGViaLanczos
     A::Function
     b
     x
     r0
+    r
     p      # The last conjugate directions
-    iter_count::Int64
+    itr::Int64
     il::IterativeLanczos
     
-    function IterativeCGViaArnoldi(A::Function, b, x0=nothing)
+    function IterativeCGViaLanczos(A::Function, b, x0=nothing)
         this = new()
         this.A = A;
         this.b = b;
         this.x = x0 === nothing ? b : x0
         this.r0 = b - A(this.x)
-        this.p = r0/norm(r0)  # matches with lancozos q vector. 
+        # this.r = this.r0
         this.il = IterativeLanczos(A, this.r0, store_Q=2)
+        this.p = this.il.Q[1]  # conjugate direction directly come from Q from lanczos
+        this.itr = 0
         return this
     end
 
-    function IterativeCGViaArnoldi(A::AbstractArray{T}, b::AbstractArray{T}) where {T<:Real}
-        return IterativeCGViaArnoldi((x)-> A*x, b)
+    function IterativeCGViaLanczos(A::AbstractArray{T}, b::AbstractArray{T}) where {T<:Real}
+        return IterativeCGViaLanczos((x)-> A*x, b)
     end
 
 end
 
 # Operator Override
-function (this::IterativeCGViaArnoldi)()
+function (this::IterativeCGViaLanczos)()
     il = this.il
-    if this.iter_count == 0 # The first iteration where conjugate direction is literally r0. 
+    if this.itr == 0 # The first iteration where conjugate direction is literally r0. 
         il()
-        a = dot(this.p, this.p)/il.betas[1]  # step size
-        this.x -= a*norm(this.r0)*this.p
-        return this.alphas[1]  # 2 norm of the residual. 
+        a = dot(this.p, this.p)/il.alphas[end]  # step size
+        this.x += a*this.r0
+        # Get residual using info from lancozos... 
+        this.itr += 1
+        return il.alphas[1]  # 2 norm of the residual. 
     end
     # q = ih()
     # compute the L matrix for the LDL. 
 
-    this.iter_count += 1
+    this.itr += 1
 end
 
+A = rand(3,3)
+A = A*A'
+b = rand(3)
+cg = IterativeCGViaLanczos(A, b)
+cg();
+il = cg.il
---
+++ Subspace_Projection_Method/testing_cg_via_lanczos.jl
@@ -0,0 +1,10 @@
+include("iterative_conjugate_gradient_via_lanczos.jl")
+using Test 
+using Logging
+
+@testset "Testing the Iterative CG via Lanczos" begin
+    
+end
+
+
+
