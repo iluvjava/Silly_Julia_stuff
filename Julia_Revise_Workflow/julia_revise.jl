b8d915252dc1ea4dbc2eada2f1729bf87a2e143c
Alto Legato <victor1301166040@gmail.com>
2022-06-07 20:02:14 -0700

    >w<

---
+++ Julia_Revise_Workflow/julia_revise.jl
@@ -0,0 +1,3 @@
+### Revise is for package developement, where people need to change things in the package without relaoding the sessions. 
+### https://timholy.github.io/Revise.jl/stable/#Other-Revise-workflows-1
+### How attractive is it? I don't know. 
---
+++ Julia_Revise_Workflow/module_change_trick.jl
@@ -0,0 +1,13 @@
+### https://discourse.julialang.org/t/redefine-struct-when-working-with-repl/25942
+
+module M
+    mutable struct MyStruct
+        attribute1::Int
+        attribute2::Int
+        attribute3::Int # Change attribute here and loading it won't end the REPL Sessions. 
+        function MyStruct() 
+        return new() end
+    end
+    export MyStruct
+end
+
